80b931b0a5c635780c7aa1ef63e8e6f11047ee88
Kitasho Devid <124248877+Devi2023@users.noreply.github.com>
2024-07-02 17:17:46 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,13 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Custom JavaScript can be added here
 });
-// Add this script to your HTML file
-    document.addEventListener("DOMContentLoaded", function() {
-        // Trigger hover effect on page load
-        document.querySelectorAll(".project-item").forEach(function(item) {
-            item.classList.add("hover");
-        });
-    });
-// Example with Lozad.js
-const observer = lozad('.project-item');
-observer.observe();
+
